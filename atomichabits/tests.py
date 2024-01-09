fe0a0d6ab37981e08855b6518b78494cfe8418c1
Valentin <4267817@mail.ru>
2024-01-08 18:33:42 -0800

    add 7_coursework

---
+++ atomichabits/tests.py
@@ -0,0 +1,150 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from atomichabits.models import Habit
+
+
+class HabitTestCase(APITestCase):
+    def setUp(self):
+        self.habit = Habit.objects.create(
+            place="test_place", action="test_action", is_pleasant=True
+        )
+
+    def test_habit_create(self):
+        data = {
+            "place": "test_place_create",
+            "action": "test_action_create",
+        }
+
+        response = self.client.post(
+            reverse("atomichabits:habit_create"), data=data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.assertEqual(Habit.objects.all().count(), 2)
+
+    def test_habit_update(self):
+        data = {
+            "place": "test_place_update",
+            "action": "test_action_update",
+        }
+
+        url = reverse("atomichabits:habit_update", args=[self.habit.pk])
+        response = self.client.put(url, data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_habit_deletion(self):
+        url = reverse("atomichabits:habit_delete", args=[self.habit.pk])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_time_to_complete_validation(self):
+        data = {
+            "place": "test_time_to_complete_validation",
+            "action": "do something",
+            "time_to_complete": 150,
+        }
+
+        response = self.client.post(
+            reverse("atomichabits:habit_create"), data=data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(
+            response.json(),
+            {
+                "time_to_complete": [
+                    "Время выполнения привычки должно быть не более 120 сек."
+                ]
+            },
+        )
+
+    def test_frequency_validation(self):
+        data = {
+            "place": "test_frequency_validation",
+            "action": "do something",
+            "frequency": 8,
+        }
+
+        response = self.client.post(
+            reverse("atomichabits:habit_create"), data=data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(
+            response.json(),
+            {
+                "frequency": [
+                    "Нельзя выполнять привычку реже, чем 1 раз в 7 дней."
+                ]
+            },
+        )
+
+    def test_related_habit_validation(self):
+        self.habit = Habit.objects.create(
+            place="uuu", action="uuu", is_pleasant=False
+        )
+
+        data = {"place": "home", "action": "do something", "related_habit": 2}
+
+        response = self.client.post(
+            reverse("atomichabits:habit_create"), data=data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(
+            response.json(),
+            {"related_habit": ["Связанная привычка должна быть полезной."]},
+        )
+
+    def test_rel_habit_and_award_validation(self):
+        data = {
+            "place": "test_rel_habit_and_award_validation",
+            "action": "do something",
+            "award": "eat something",
+            "related_habit": 1,
+        }
+
+        response = self.client.post(
+            reverse("atomichabits:habit_create"), data=data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(
+            response.json(),
+            {
+                "non_field_errors": [
+                    "Связанную привычку и вознаграждение нельзя выбирать одновременно"
+                ]
+            },
+        )
+
+    def test_is_pl_and_award_or_rel_h_validation(self):
+        data = {
+            "place": "test_is_pl_and_award_or_rel_h_validation",
+            "action": "do something",
+            "award": "eat something",
+            "is_pleasant": True,
+        }
+
+        response = self.client.post(
+            reverse("atomichabits:habit_create"), data=data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        self.assertEqual(
+            response.json(),
+            {
+                "non_field_errors": [
+                    "У приятной привычки не может быть вознаграждения или связанной привычки."
+                ]
+            },
+        )
---
+++ config/settings.py
@@ -0,0 +1,135 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.postgres",
+    "drf_yasg",
+    "atomichabits",
+    "users",
+    "rest_framework",
+    "corsheaders",
+    "rest_framework_simplejwt",
+    "django_celery_beat",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "pgdb",
+        "PORT": "5432",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "ru-ru"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+AUTH_USER_MODEL = "users.User"
+
+LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/users/"
+
+STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=150),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+
+# URL-адрес брокера результатов, также Redis
+
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "UTC"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
